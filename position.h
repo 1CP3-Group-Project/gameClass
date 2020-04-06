ec57eb96f798674ff51d40bfa5e58ab40cc48d1b
queeniel <62863600+queeniel@users.noreply.github.com>
2020-04-06 17:48:59 -0400

    Create position.h

---
+++ position.h
@@ -0,0 +1,13 @@
+//position.h 
+//header
+#ifndef POSITION_H 
+#define POSITION_H 
+
+//created a structure for the coordinates
+//abstract data type
+struct position {
+	int col;
+	int row;
+};
+
+#endif
